9304e89347b12b826fac88157353d204aa779f6d
derikhill <derikhill05@gmail.com>
2014-11-23 21:17:47 -0600

    JS added

--- js/app.js
+++ js/app.js
@@ -1,14 +1,42 @@
 
 $(document).ready(function(){
 	
-	var newNumber 
-	var computerChoice
-	var userChoice
-	var guessCount 
+	var randomNumber;
+	var guessFlag;
+	var guessCount;
+	var userChoice;
+	var found = false;
+
+	newGame();
+
+  	//--- Creating new game ---//
+  	$(".new").click(function(event){
+  		event.preventDefault();
+  		newGame();
+  	});
+
+	//--- Input box ---//
+	$("form").submit(function(event){
+		event.preventDefault();
+			if (userChoice = $('#userGuess').val()) {
+			console.log("User chose = "+ userChoice);
+			$("#userGuess").val('');
+			guessFlag = checkChoice(userChoice);
+			if (!guessFlag) {
+				guessCount++;
+				setCount(guessCount);
+				$("ul#guessList").append("<li>" + userChoice + "</li>");
+				guessFlag = checkTemperature(Math.abs(randomNumber - userChoice));
+			};
+		} else {
+			setFeedback("You've already won. Start a new game.");
+		};
+  	});
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
+
   	});
 
   	/*--- Hide information modal box ---*/
@@ -16,46 +44,68 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	// New Game
-  	$(document).on("click", ".new", function(){
-  		$("#count").val('0');
-  		$('ul#guessList li').remove();
-  		newNumber = randomNumber();
-  		console.log('new number');
-  	});
-
-  		$('form').submit(function(event) {
-  		var userGuess = $('#userGuess').val();
-  		$('ul#guessList').prepend('<li>' + userGuess + '</li>');
-  		$('#userGuess').val('');
-  		$("#count")
-  	});
-/*$('#userGuess').change(function(){
-	var obj=$(this)
-		var text=obj.val();
-		var list='ul#guessList';
-		var item=$('<li />');
-		if(text !='') {
-			item.text(text).prependTo(list);
-		}
-			obj.val('');
-		});*/
-var compare = function(userGuess, computerChoice) {
-	if (userGuess == computerChoice) {
-		return "Yes! You guessed it! Let's play again."
+	//--- New Game ---//
+	function newGame() {
+		guessCount = 0;
+		found = false;
+		$("ul#guessList li").remove();
+		setFeedback("Make your Guess!");
+		setCount(guessCount);
+		randomNumber = computerChoice();
+		$("#userGuess").val('');
 	}
-	/*else if() {
-		return "Your guess is cold."
-	}*/
 
-}
+	//--- Random computer number ---//
+	function computerChoice() {
+		var computerChoice = Math.floor(Math.random() * 101);
+			console.log('new secret number ' + computerChoice);
+			return computerChoice;
+	}
 
-function randomNumber () {
+	//--- Guess count ---//
+	function setCount(count) {
+		$('#count').text(guessCount);
+	}
 
-var computerChoice = Math.floor(Math.random() * 101);
-		console.log('new secret number' + computerChoice);
-		return computerChoice;
-}
+	//--- Setting user parameters ---//
+	function checkChoice(userGuess) {
+		if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
+			setFeedback("Sorry, you must enter a number between 1 and 100");
+			return true;
+		}
+	}
+
+	//--- Set Feedback Message---//
+	function checkTemperature(guessDifference) {
+
+		if (guessDifference == 0) {
+			setFeedback("Yeah! You got it!!");
+		} 
+		else if (guessDifference <= 5) {
+			setFeedback("You're so close it's burning!");
+		} 
+		else if (guessDifference <= 10){
+			setFeedback("You're getting hot!");
+		} 
+		else if (guessDifference>=10 && guessDifference <= 20) {
+			setFeedback("Your Guess is getting Warm!");
+		} 
+		else if (guessDifference>=20 && guessDifference <= 30) {
+			setFeedback("Your guess is cold!");
+		} 
+		else if (guessDifference>=30 && guessDifference <= 40) {
+			setFeedback("Your guess is really cold!");
+		} 
+		else {
+			setFeedback("Your guess is freezing!");
+		}
+
+	}
+
+	/*--- Set the feedback ---*/
+	function setFeedback(feedback) {
+		$('#feedback').text(feedback);
+	}
 });
 
 
